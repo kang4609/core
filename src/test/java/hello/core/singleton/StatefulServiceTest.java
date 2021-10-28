2e843a7c9a7ff1a8813d2c24e2f9ec6303324162
kang4609 <kang4609@gmail.com>
2021-10-28 22:05:07 +0900

    commit

--- src/test/java/hello/core/singleton/StatefulServiceTest.java
+++ src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -1,11 +1,10 @@
 package hello.core.singleton;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class StatefulServiceTest {
     @Test
@@ -21,7 +20,7 @@ class StatefulServiceTest {
       // int price =  statefulService1.getPrice();
         System.out.println("price1 = " + price1);
         System.out.println("price2 = " + price2);
-        Assertions.assertThat(statefulService1.getPrice()).isEqualTo(20000);
+        assertThat(statefulService1.getPrice()).isEqualTo(20000);
     }
 
     static class  TestConfig {
